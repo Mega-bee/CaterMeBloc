719380391b6339c8f08d54d9a051fb7ef4ef96ab
jadsaidd <jadsaidd@gmail.com>
2022-10-07 11:00:02 +0300

    jad

--- lib/home_page/ui/state/homepage_sucess.dart
+++ lib/home_page/ui/state/homepage_sucess.dart
@@ -5,9 +5,7 @@ import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
 import '../../../abstracts/states/state.dart';
-import '../../../custom/model/OrderModel.dart';
 import '../../../hive/hive.dart';
 import '../../../utils/Colors/colors.dart';
 import '../../response/homepage_response.dart';
@@ -44,110 +42,229 @@ class HomePageSuccess extends States {
     //   ),
     // );
 
-    return SafeArea(
-      child: RefreshIndicator(
-        color: redColor,
-        onRefresh: () async {
-          homepageState.getHome();
-        },
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 9.0, right: 15.0, top: 15.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CarouselSlider.builder(
-                    options: CarouselOptions(
-                        viewportFraction: 1,
-                        height: 250,
-                        autoPlay: true,
-                        enlargeCenterPage: true,
-                        enableInfiniteScroll: true,
-                        onPageChanged: (index, reason) {
-                          _currentIndex = index;
-                          homepageState.refresh();
-                        }),
-                    itemCount: homepage.events!.services!.length,
-                    itemBuilder: (BuildContext context, int itemIndex,
-                            int pageViewIndex) =>
-                        CarouselImageSlider(
-                          homepage: homepage,
-                          itemIndex: itemIndex,
-                        )),
-                SizedBox(
-                  height: 55,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor:
+        ThemeHelper().getisDark() ? Colors.black : Colors.white,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 8, right: 8),
+          child: IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Settings()),
+                );
+              },
+              icon: ImageIcon(
+                AssetImage(ImageAsset.user.toString()),
+                color: ThemeHelper().getisDark() ? Colors.white : Colors.black,
+              )),
+        ),
+        centerTitle: true,
+        title: Padding(
+          padding: const EdgeInsets.only(top: 0),
+          child: Image.asset(
+            ImageAsset.logo2,
+            fit: BoxFit.contain,
+            height: 65,
+          ),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () => null,
+              icon: ImageIcon(
+                AssetImage(ImageAsset.search.toString()),
+                color: ThemeHelper().getisDark() ? Colors.white : Colors.black,
+              ))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: SizedBox(
+                height: 250,
+                width: MediaQuery.of(context).size.width,
+                child: CarouselSlider.builder(
+                  options: CarouselOptions(
+                    viewportFraction: 1,
+                    autoPlay: true,
+                  ),
+                  itemCount: List1.length,
+                  itemBuilder: (context, itemIndex, realIndex) {
+                    return List1widget(list1key: List1[itemIndex]);
+                  },
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Nearest shop",
-                      textAlign: TextAlign.start,
-                      style: GoogleFonts.poppins(
-                        fontStyle: FontStyle.normal,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 0,
-                    ),
-                    Text(
-                      "You don't mind which shop, just order from the nearest",
-                      style: GoogleFonts.poppins(
-                          fontStyle: FontStyle.normal,
-                          fontSize: 12,
-                          color: Colors.grey),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 19,
-                ),
-                Container(
-                  height: 230,
-                  // color: Colors.black.withOpacity(0.8),
-
-                  child: GridView.builder(
-                    physics: BouncingScrollPhysics(),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: (2 / 4),
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 0,
-                    ),
-                    controller: _controller,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: homepage.destinations!.length,
-                    itemBuilder: (context, index) {
-                      return GridViewDetails(
-                        homepage: homepage,
-                        index: index,
-                      );
-                    },
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 10, left: 10),
+              child: Text(
+                '  Menus',
+                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 10, left: 10),
+              child: Text(
+                '   Choose one of our special menus',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 10, left: 10),
+              child: SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: GridView.builder(
+                  shrinkWrap: false,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: List2.length,
+                  itemBuilder: (context, index) {
+                    return List2widget(
+                        list2key: List2[index],
+                        ontap: () {
+                          List2.forEach((element) {
+                            print(element);
+                            element.check == !element.check! ;
+                          });
+                          List2[index].check == true;
+                          print(List2.where((element) => element.check=false));
+                        });
+                  },
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 3 / 4.7,
                   ),
                 ),
-                SizedBox(
-                  height: 50,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 10, left: 10),
+              child: Text(
+                '  Add-ons',
+                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 10, left: 10),
+              child: Text(
+                '   Extra add-ons to your special menu',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SizedBox(
+                height: 150,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: List3.length,
+                  itemBuilder: (context, index) {
+                    return List3widget(list3key: List3[index]);
+                  },
                 ),
-                Column(
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 10, left: 10),
+              child: Text(
+                '  Flowers',
+                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 10, left: 10),
+              child: Text(
+                '   Roses are red,violets are blue',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SizedBox(
+                height: 150,
+                width: MediaQuery.of(context).size.width,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: List4.length,
+                  itemBuilder: (context, index) {
+                    return List4widget(list4key: List4[index]);
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 130,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 8, right: 8),
+        child: SizedBox(
+          width: 400.0,
+          height: 68.0,
+          child: FloatingActionButton.extended(
+            backgroundColor: PrimaryColor,
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                backgroundColor: Colors.transparent,
+                builder: (context) => Modelsheet(),
+              );
+            },
+            label: Container(
+              width: 315,
+              child: Row(
+
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    ListView.builder(
-                        physics: NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: homepage.destinationWithPlaces!.length,
-                        itemBuilder: (context, index) {
-                          final model = homepage.destinationWithPlaces![index];
-                          return DestinationWithPlacesList(
-                            homepage: homepage,
-                            model: model,
-                            index: index,
-                          );
-                        }),
-                  ],
-                ),
-              ],
+                    Icon(Icons.calendar_month),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'What s your event?',
+                          style: TextStyle(fontSize: 11),
+                        ),
+                        Text(
+                          'Date,Places,guests...',
+                          style: TextStyle(fontSize: 11),
+                        )
+                      ],
+                    ),
+                    Spacer(),
+                    Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(200),
+                          color: Colors.white,
+                        ),
+                        height: 30,
+                        width: 30,
+                        child: Center(
+                            child: Icon(
+                              Icons.keyboard_arrow_up_outlined,
+                              color: PrimaryColor,
+                              size: 30,
+                            ))),
+                  ]),
             ),
           ),
         ),
